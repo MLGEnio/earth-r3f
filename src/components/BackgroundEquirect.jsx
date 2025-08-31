1b367fcba7f3e035dac5c43e6bba01c1e9f725ba
GlendiFojt <glendifojt@proton.me>
2025-08-31 20:15:05 +0200

    background image changes

--- src/components/BackgroundEquirect.jsx
+++ src/components/BackgroundEquirect.jsx
@@ -1,29 +1,34 @@
-// BackgroundEquirect.jsx
 import { useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 
 export default function BackgroundEquirect({ url, setEnv = false }) {
-    const { scene } = useThree();
+    const { scene, gl } = useThree();
     const tex = useTexture(url);
 
     useEffect(() => {
-        tex.mapping = THREE.EquirectangularReflectionMapping;
         tex.colorSpace = THREE.SRGBColorSpace;
+        tex.mapping = THREE.EquirectangularReflectionMapping;
+
+        // Quality knobs
+        tex.anisotropy = gl.capabilities.getMaxAnisotropy();
+        tex.minFilter = THREE.LinearMipmapLinearFilter;
+        tex.magFilter = THREE.LinearFilter;
+        tex.generateMipmaps = true;
+        tex.needsUpdate = true;
 
         const prevBg = scene.background;
         const prevEnv = scene.environment;
 
-        scene.background = tex;      // panoramic sky that rotates with camera
-        if (setEnv) scene.environment = tex; // optional PBR reflections
+        scene.background = tex;          // high-res pano
+        if (setEnv) scene.environment = tex; // careful: env will be PMREM'd (lower res)
 
         return () => {
             scene.background = prevBg;
             scene.environment = prevEnv;
-            tex.dispose?.();
         };
-    }, [scene, tex, setEnv]);
+    }, [scene, gl, tex, setEnv]);
 
     return null;
 }
